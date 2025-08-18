6011d9351675e0074e07bae6f3951d4ade682f64
CH3J0 <pepedoy1213@gmail.com>
2025-08-17 22:52:04 -0600

    Definir función guardar_puntaje (estructura básica)

--- resultados.py
+++ resultados.py
@@ -1,3 +1,7 @@
 # resultados.py
 # Este módulo se encargará de guardar y mostrar los puntajes del juego de trivia.
 # Usaremos un archivo llamado 'puntajes.txt' para almacenar los resultados.
+
+def guardar_puntaje(nombre, puntaje):
+    # Esta función guardará el nombre y puntaje en el archivo puntajes.txt
+    pass
