f4d343cee4cbb8022a1f38ac94ffe6db8361dd53
Arenzzzz <varenzp024@gmail.com>
2025-08-17 19:25:55 -0600

    COMMIT 3: Añadir preguntas FÁCIL

--- CURADOR DE PREGUNTAS.py
+++ CURADOR DE PREGUNTAS.py
@@ -2,7 +2,18 @@
 # Autor: ARENZ PELÁEZ - 1556425
 
 preguntas = [
-    {'FÁCIL':[]},
+    {'FÁCIL':[
+        {'¿Cuál es la capital de Francia?':{'Respuesta':'París', 'Opciones':['Madrid', 'París', 'Berlín', 'Roma']}},
+        {'¿Cuánto es 2 + 2?':{'Respuesta':'4', 'Opciones':['1', '2', '3', '4']}},
+        {'¿Qué animal dice "miau"?':{'Respuesta':'Gato', 'Opciones':['Gato', 'Perro', 'Gallina', 'Rana']}},
+        {'¿Qué día sigue al Lunes?':{'Respuesta':'Martes', 'Opciones':['Miércoles', 'Domingo', 'Viernes', 'Martes']}},
+        {'¿Cuántos meses tiene un año?':{'Respuesta':'12', 'Opciones':['10', '12', '11', '9']}},
+        {'¿Cuál es el color de una manzana roja?':{'Respuesta':'Rojo', 'Opciones':['Rojo', 'Verde', 'Azul', 'Amarillo']}},
+        {'¿Qué planeta es conocido como el planeta rojo?':{'Respuesta':'Marte', 'Opciones':['Tierra', 'Marte', 'Venus', 'Júpiter']}},
+        {'¿Cuál es el número después del 9?':{'Respuesta':'10', 'Opciones':['10', '11', '8', '12']}},
+        {'¿Qué gas respiramos principalmente?':{'Respuesta':'Oxígeno', 'Opciones':['Oxígeno', 'Hidrógeno', 'Nitrógeno', 'Helio']}},
+        {'¿Cuál es la marca de tecnología con una manzana por logotipo?':{'Respuesta':'Apple', 'Opciones':['Tesla', 'Microsoft', 'Facebook', 'Apple']}}
+        ]},
     {'MEDIA':[]},
     {'DIFÍCIL':[]},
     {'EXTRA':[]}
